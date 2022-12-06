cbe77cae8f5445d7be9d6cb596d80806c232ddbb
Avinash M <avinashmohan2001@gmail.com>
2022-12-06 13:59:52 +0530

    Add Interoperability - Scanner.kt

---
+++ 15.Interoperability with Java/Using Java libraries in Kotlin/1.Scanner.kt
@@ -0,0 +1,61 @@
+import java.util.Scanner
+
+fun main() {
+
+    // backticks (`) are used for "Escaping Java identifiers that are keywords in Kotlin"
+    // Here 'in' is a keyword in Kotlin, so we are enclosing the Java 'in' using backticks
+    val sc = Scanner(System.`in`)
+
+    println(sc::class) // class java.util.Scanner
+    println()
+
+    print("Enter an integer: ")
+    val intVal = sc.nextInt()
+    println("You entered a value of type '${intVal::class}'\n")
+
+    print("Enter a floating point input: ")
+    val floatVal = sc.nextFloat()
+    println("You entered a value of type '${floatVal::class}'\n")
+
+    print("Enter a double type input: ")
+    val doubleVal = sc.nextDouble()
+    println("You entered a value of type '${doubleVal::class}'\n")
+
+    print("Enter a character input: ")
+    val charVal = sc.next()[0]
+    println("You entered a value of type '${charVal::class}'\n")
+
+    print("Enter a String input: ")
+    val stringVal = sc.next()
+    println("You entered a value of type '${stringVal::class}'\n")
+
+    print("Enter a boolean input: ")
+    val booleanVal = sc.nextBoolean()
+    println("You entered a value of type '${booleanVal::class}'\n")
+
+    /* Output
+
+    class java.util.Scanner
+
+    Enter an integer: 9
+    You entered a value of type 'class kotlin.Int'
+
+    Enter a floating point input: 36.99
+    You entered a value of type 'class kotlin.Float'
+
+    Enter a double type input: 36.90
+    You entered a value of type 'class kotlin.Double'
+
+    Enter a character input: n
+    You entered a value of type 'class kotlin.Char'
+
+    Enter a String input: ash
+    You entered a value of type 'class kotlin.String'
+
+    Enter a boolean input: true
+    You entered a value of type 'class kotlin.Boolean'
+
+     */
+
+    // Though we get input using the Scanner class of Java, the variables types are of Kotlin
+}
