4c493e283639f1f354a2f6a56f34a39f1d957681
Avinash M <avinashmohan2001@gmail.com>
2022-12-26 13:47:20 +0530

    Delete 15.Interoperability with Java/Using Kotlin in Java directory

--- 15.Interoperability with Java/Using Kotlin in Java/Calculator.kt
+++
@@ -1,9 +0,0 @@
-// @JvmName defines the name of the target in the bytecode, which is also the name we can use when referring to the target from Java.
-// https://www.baeldung.com/kotlin/jvm-annotations
-@file:JvmName("Calculator")
-
-fun add(num1: Int, num2: Int): Int = num1 + num2
-
-fun mul(num1: Int, num2: Int): Int = num1 * num2
-
-fun sub(num1: Int, num2: Int): Int = num1 - num2
